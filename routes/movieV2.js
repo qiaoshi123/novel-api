c0c1852f9fbff2044d3eedb7c1195ba2fbd0e7e6
qiaoshi <banya@ibantang.com>
2019-10-20 15:33:30 +0800

    test

--- routes/movieV2.js
+++ routes/movieV2.js
@@ -6,7 +6,6 @@ let request = require('request');
 let common = require('../common/common.json'); // 引用公共文件
 let router = express.Router();
 //首页tab
-// https://movie.redatao.com/program/api/m/type-hot
 let tabs = [
     {name: "热门推荐", path: '/api/m/type-hot', list: [], page: 0},
     {name: "电影", path: '/api/m/type-m', list: [], page: 0},
@@ -16,10 +15,10 @@ let tabs = [
 ];
 /**
  * tabs
+ * get请求
+ * /v2/movie/index
  */
 router.get('/index', function (req, res, next) {
-    // res.success({...tabs})
-
     let pAry = [];
     tabs.forEach((v, i) => {
         let data = {
@@ -32,20 +31,26 @@ router.get('/index', function (req, res, next) {
         pAry.push(p)
     });
     Promise.all(pAry).then(result => {
-        result.forEach((v,i)=>{
+        result.forEach((v, i) => {
             tabs[i].list = v || [];
         });
         res.success({tabs});
-    }).catch(e=>{
-        console.log('error',e);
+    }).catch(e => {
+        console.log('error', e);
         res.success({tabs})
     })
 });
 /**
- * 获取tab数据
- * @type {Router}
+ * 获取指定tab数据
+ * post请求
+ * /v2/movie/listByType
+ * {
+ *     num:21,
+ *     page:0,
+ *     type:'热门推荐',
+ *     path:'/api/m/type-hot' '/api/m/type-m' 等
+ * }
  */
-
 router.post('/listByType', function (req, res, next) {
     let form = {
         type: req.body.type,
@@ -67,6 +72,83 @@ router.post('/listByType', function (req, res, next) {
 });
 
 /**
+ * 获取banner轮播数据
+ * get请求
+ * /v2/movie/banner
+ */
+router.get('/banner', (req, res, next) => {
+    let url = `${common.MOVIEV2}/api/m/search-circle`;
+    request.get(url, (err, response, body) => {
+        if (err) {
+            return console.error('upload failed:', err);
+        }
+        body = JSON.parse(body);
+        let list = body.circleList;
+        res.success({list})
+    })
+});
+
+/**
+ * 获取搜索 筛选条件
+ * /v2/movie/search
+ * {
+ *      type: '不限', year: '不限',
+        country: '不限',
+        name: ''
+ * }
+ */
+router.post('/search', (req, res, next) => {
+    let form = {
+        type: '不限', year: '不限',
+        country: '不限',
+        name: ''
+    };
+    Object.assign(form, req.body);
+    let url = `${common.MOVIEV2}/api/m/search`;
+    request.post({url, form}, function optionalCallback(err, httpResponse, body) {
+        if (err) {
+            return console.error('upload failed:', err);
+        }
+        body = JSON.parse(body);
+        let list = [];
+        if (body.code == 200) {
+            list = body.data;
+        }
+        res.success({list})
+    });
+});
+
+
+/**
+ * 获取搜索页 筛选 条件
+ * /v2/movie/filter
+ *
+ */
+router.get('/filter', (req, res, next) => {
+    let url = `${common.MOVIEV2}/api/m/search-data`;
+    request.get(url, function optionalCallback(err, httpResponse, body) {
+        if (err) {
+            return console.error('upload failed:', err);
+        }
+        body = JSON.parse(body);
+        let filters = {
+            ...body
+        };
+        request.post({url:`${common.LOCAL}/v2/movie/search`,form:{}},(err,response,body)=>{
+            body = JSON.parse(body);
+            let list = [];
+            if(body.status == 1){
+                list = body.data.list || [];
+            }
+            res.success({filters,list})
+        });
+    });
+
+});
+
+
+
+/**
  *内部掉用 封装；
  */
 function requestCustom(data) {
@@ -84,4 +166,5 @@ function requestCustom(data) {
 
     })
 }
+
 module.exports = router;
