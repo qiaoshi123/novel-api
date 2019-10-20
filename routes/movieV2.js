23c6bec974d98a82e6518b467a879375b04daf06
qiaoshi <banya@ibantang.com>
2019-10-20 14:29:34 +0800

    test

--- routes/movieV2.js
+++ routes/movieV2.js
@@ -8,29 +8,49 @@ let router = express.Router();
 //首页tab
 // https://movie.redatao.com/program/api/m/type-hot
 let tabs = [
-    {name:"热门推荐",path:'/program/api/m/type-hot',list:[],page:0},
-    {name:"电影",path:'/program/api/m/type-m',list:[],page:0},
-    {name:"电视剧",path:'/program/api/m/type-s',list:[],page:0},
-    {name:"动漫",path:'/program/api/m/type',list:[],page:0},
-    {name:"综艺",path:'/program/api/m/type',list:[],page:0}
+    {name: "热门推荐", path: '/api/m/type-hot', list: [], page: 0},
+    {name: "电影", path: '/api/m/type-m', list: [], page: 0},
+    {name: "电视剧", path: '/api/m/type-s', list: [], page: 0},
+    {name: "动漫", path: '/api/m/type', list: [], page: 0},
+    {name: "综艺", path: '/api/m/type', list: [], page: 0}
 ];
 /**
  * tabs
  */
 router.get('/index', function (req, res, next) {
+    // res.success({...tabs})
 
-    res.success({...tabs})
+    let pAry = [];
+    tabs.forEach((v, i) => {
+        let data = {
+            type: v.name,
+            page: v.page,
+            num: 21,
+            path: v.path
+        };
+        let p = requestCustom(data);
+        pAry.push(p)
+    });
+    Promise.all(pAry).then(result => {
+        result.forEach((v,i)=>{
+            tabs[i].list = v || [];
+        });
+        res.success({...tabs});
+    }).catch(e=>{
+        console.log('error',e);
+        res.success({...tabs})
+    })
 });
 /**
  * 获取tab数据
  * @type {Router}
  */
 
-router.post('/listByType',function (req,res,next) {
+router.post('/listByType', function (req, res, next) {
     let form = {
-        type:req.body.type,
-        page:req.body.page || 0,
-        num:req.body.num || 21
+        type: req.body.type,
+        page: req.body.page || 0,
+        num: req.body.num || 21
     };
     let url = `${common.MOVIEV2}${req.body.path}`;
     request.post({url, form}, function optionalCallback(err, httpResponse, body) {
@@ -46,6 +66,25 @@ router.post('/listByType',function (req,res,next) {
     });
 });
 
+/**
+ *
+ * @param url
+ * @param form
+ * @returns {Promise<any>}
+ */
+function requestCustom(data) {
+    return new Promise((resolve, reject) => {
+        request.post({
+            url: `http://localhost:6060/v2/movie/listByType`,
+            form: data
+        }, function optionalCallback(err, httpResponse, body) {
+            if (err) {
+                return console.error('upload failed:', err);
+            }
+            let bodyObj = JSON.parse(body);
+            resolve(bodyObj.data.list || [])
+        });
 
-
+    })
+}
 module.exports = router;
