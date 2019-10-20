81bce04f56b7d4bd8886b1c373b6f27a481bb394
qiaoshi <banya@ibantang.com>
2019-10-20 10:28:43 +0800

    listByType

--- routes/movieV2.js
+++ routes/movieV2.js
@@ -14,10 +14,38 @@ let tabs = [
     {name:"动漫",path:'/program/api/m/type',list:[],page:0},
     {name:"综艺",path:'/program/api/m/type',list:[],page:0}
 ];
+/**
+ * tabs
+ */
 router.get('/index', function (req, res, next) {
 
     res.success({...tabs})
 });
+/**
+ * 获取tab数据
+ * @type {Router}
+ */
+
+router.post('/listByType',function (req,res,next) {
+    let form = {
+        type:req.body.type,
+        page:req.body.page || 0,
+        num:req.body.num || 21
+    };
+    let url = `${common.MOVIEV2}${req.body.path}`;
+    request.post({url, form}, function optionalCallback(err, httpResponse, body) {
+        if (err) {
+            return console.error('upload failed:', err);
+        }
+        body = JSON.parse(body);
+        let list = [];
+        if (body.code == 200) {
+            list = body.data;
+        }
+        res.success({list})
+    });
+});
+
 
 
 module.exports = router;
