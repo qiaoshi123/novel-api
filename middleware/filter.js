fe5fb3fdff56a9cc6ee088b6db3570ac193ee736
qiaoshi <banya@ibantang.com>
2019-10-22 16:14:54 +0800

    test

--- middleware/filter.js
+++ middleware/filter.js
@@ -1,21 +1,71 @@
+const moment = require('moment');
 
 function filter() {
-    return function(req, res, next) {
-        res.success =(data)=> {
+    return function (req, res, next) {
+        let config = getConfig(req);
+        res.success = (data) => {
             res.send({
-                status:1,
-                data,
-                msg:''
+                status: 1,
+                data: {...data, config},
+                msg: ''
             })
         };
-        res.fail = (data) =>{
+        res.fail = (data) => {
             res.send({
-                status:500,
+                status: 500,
                 data,
-                msg:'api错误'
+                msg: 'api错误'
             })
         };
         next();
+    }
+}
+
+function getConfig(req) {
+    let version = req.body.version || req.query.version;
+    let appId = req.body.appId || req.query.appId;
+    let config = require(`../common/${appId}.json`);
+    let appInfo = {
+        path: '',// 跳转首页地址
+        is_hit: '',//是否命中审核
+        is_show_player: 1,//是否显示播放入口
+        is_other_app_player: 1,//是否跳转至其他小程序播放
+    };
+
+    if (config.LIMIT == 1) {
+        let flag = checkTime(config.STARTTIME, config.ENDTIME);
+        if (flag) {
+            appInfo.path = config.MOVIE;
+
+        } else {
+            appInfo.path = config.PATH;
+        }
+    } else {
+        appInfo.path = config.PATH;
+    }
+
+    appInfo.is_hit = config.VERSION == version ? 1 : 0;
+    //是否显示播放入口
+    if (appInfo.is_hit) {
+        appInfo.is_show_player = 0;
+    } else {
+        appInfo.is_show_player = 1;
+    }
+
+    //是否在当前小程序播放
+    appInfo.is_other_app_player = config.OTHERAPPPLAYER == 1 ? 1 : 0;
+
+
+    return appInfo;
+}
+
+//校验当前是否处于某个 时间段;
+function checkTime(start, end) {
+    const hour = moment().hour();
+    if (hour >= start || hour < end) {
+        return true;
+    } else {
+        return false;
     }
 }
 
--- routes/wxenable.js
+++ routes/wxenable.js
@@ -11,49 +11,28 @@ const moment = require('moment');
  * /wxenable/landing?v=1.0.1
  */
 router.get('/landing', function (req, res, next) {
-        let v = req.query.v;
-        let path;
-        let hit;
-        //如果版本是审核版本
-        if (v && v == miniProgramConfig.VERIFYVERSION) {
-            path = miniProgramConfig.SHOPPATH;
-            hit = 1;
-        }else{
-            let flag = canGoMovie(miniProgramConfig.MOVIEOPENTIME,miniProgramConfig.MOVIECLOSETIME);
-            if(flag){
-                path = miniProgramConfig.MOVIEPATH;
-            }else{
-                path = miniProgramConfig.SHOPPATH;
-            }
-            hit = 0;
-        }
-        res.success({path,hit})
+        res.success({})
 });
 /**
  * 修改配置
  * /wxenable/config?VERIFYVERSION=1.0.0
  */
 router.get('/config',function (req, res, next) {
-    Object.assign(miniProgramConfig,req.query);
-    fs.writeFile(path.join(__dirname,'../common/miniProgramConfig.json'), JSON.stringify(miniProgramConfig),'utf8',function(error){
+    let appId = req.query.appId;
+    let config = require(`../common/${appId}.json`);
+    delete  req.query.version;
+    delete req.query.appId;
+    Object.assign(config,req.query);
+    fs.writeFile(path.join(__dirname,`../common/${appId}.json`), JSON.stringify(config),'utf8',function(error){
         if(error){
             console.log(error);
             res.fail({});
         }
-        res.success({miniProgramConfig})
+        res.success({result:config})
     })
-
 });
 
-//校验当前是否处于某个 时间段;
-function canGoMovie(start,end) {
-    const hour = moment().hour();
-    if (hour >= start || hour < end) {
-        return true;
-    } else {
-        return false;
-    }
-}
+
 module.exports = router;
 
 
