b4a77b1defba26a7469bf80bb6c1723160f79431
qiaoshi <banya@ibantang.com>
2019-12-27 14:26:28 +0800

    test

---
+++ public/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "novel-api",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-12-27T06:14:09.867Z",    "url": "http://apidocjs.com",    "version": "0.19.0"  }});
