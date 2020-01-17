4d616afebe9eaf16870a386e2dc7bfeef6cfad32
qiaoshi <banya@ibantang.com>
2020-01-17 17:37:28 +0800

    test

--- routes/quce.js
+++ routes/quce.js
@@ -46,7 +46,7 @@ router.get('/hotwords', function (req, res, next) {
             var str = sres.text;
             var $ = cheerio.load(str);
             let list = [];
-            var reg = /\/index.php\/wetest\/index\/search\/q\/([\s\S]*)/
+            var reg = /\/index.php\/wetest\/index\/search\/q\/([\s\S]*)/;
 
             $('.search-label').each((index,item)=>{
                 var $element = $(item);
@@ -61,5 +61,35 @@ router.get('/hotwords', function (req, res, next) {
 });
 
 
+/**
+ *
+ *获取banner
+ */
+router.get('/banner',function (req, res, next) {
+    let originUrl = 'https://qc-ssl.itwlw.com/index.php/wetest/index/index';
+    superagent.get(originUrl)
+        .end(function (err, sres) {
+            // 常规的错误处理
+            if (err) {
+                return next(err);
+            }
+            var str = sres.text;
+            var $ = cheerio.load(str);
+            let list = [];
+            var reg = /\/entry.php\/url\/index\/inDrct\/id\/(\d+)\/src\/1/;
+
+            $('#topic .swiper-slide').each((index,item)=>{
+                var $element = $(item);
+                let gid = reg.exec($element.attr('data-url'))[1];
+                let obj = {
+                    img_url: $element.children('img').attr('src'),
+                    gid: gid,
+                    detail_url:$element.attr('data-url')
+                };
+                list.push(obj)
+            });
+            res.send(list);
+        });
+});
 
 module.exports = router;
