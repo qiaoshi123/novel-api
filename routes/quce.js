5358141dd15500f2db057222008766d63bec1f00
qiaoshi <banya@ibantang.com>
2020-03-13 18:16:52 +0800

    test

--- routes/quce.js
+++ routes/quce.js
@@ -59,7 +59,34 @@ router.get('/hotwords', function (req, res, next) {
             res.send(list);
         });
 });
+/**
+ * 最新素材
+ */
+router.get('/newItem',function (req, res, next) {
+    let originUrl = 'https://qc-ssl.itwlw.com/index.php/wetest/index/index';
+    superagent.get(originUrl)
+        .end(function (err, sres) {
+            // 常规的错误处理
+            if (err) {
+                return next(err);
+            }
+            var str = sres.text;
+            var $ = cheerio.load(str);
+            let list = [];
+            var reg = /\/index.php\/wetest\/index\/search\/q\/([\s\S]*)/;
+            var reg = /\/index.php\/wetest\/entry\/index\/id\/(\d+)/
 
+            $('.alert-box .a-professional').each((index,item)=>{
+                var $element = $(item);
+                let obj = {
+                    text: $element.children('span').text(),
+                    id: reg.exec($element.attr('href'))[1]
+                }
+                list.push(obj)
+            });
+            res.send(list);
+        });
+})
 
 /**
  *
