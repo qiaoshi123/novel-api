ce56ad0b9f488b689f9accbde45f1ed402251eef
qiaoshi <banya@ibantang.com>
2020-09-27 17:58:50 +0800

    test

--- routes/1026Tv.js
+++ routes/1026Tv.js
@@ -138,7 +138,7 @@ router.get('/getPlayerSource',function (req,res,next) {
     }
         puppeteer.launch({
             args: ['--no-sandbox','--disable-setuid-sandbox'],
-            headless: true, //不使用无头模式使用本地可视化
+            headless: true, //false 不使用无头模式使用本地可视化,//true使用无头模式，无界面模式；默认为有头
             // executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", //因为是yarn add puppeteer --ignore-scripts没有安装chromium，需要制定本地chromium的chrome.exe路径所在,刚才下载后解压后的全路径
             // executablePath:"/usr/share/man/man1/google-chrome.1.gz",
             //设置超时时间
