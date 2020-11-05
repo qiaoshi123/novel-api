323cc8c81ab1a39f441d92be8c0626e21367e37d
qiaoshi <banya@ibantang.com>
2020-11-05 18:39:07 +0800

    homeOtherNavIndex

--- routes/1026Tv.js
+++ routes/1026Tv.js
@@ -2,7 +2,7 @@
  * 电影 v2
  **/
 let fs = require('fs');
-let path =require('path');
+let path = require('path');
 let express = require('express');
 let router = express.Router();
 let cheerio = require('cheerio');
@@ -11,19 +11,19 @@ const puppeteer = require('puppeteer');
 const TYPE = 'cunzhangbatv';
 
 let BASEURL = '';
-if(TYPE == '1026tv'){
+if (TYPE == '1026tv') {
     BASEURL = 'https://www.1026tv.com'
 }
-if(TYPE =='cunzhangbatv'){
-    BASEURL ='https://www.cunzhangba.com'
+if (TYPE == 'cunzhangbatv') {
+    BASEURL = 'https://www.cunzhangba.com'
 }
 
 /**
- * 首页
+ * 首页 navs && 第一个nav的list
  * @type {Router}
  */
-router.get('/homeIndex',(req,res)=>{
-    if(TYPE == 'cunzhangbatv'){
+router.get('/homeIndex', (req, res) => {
+    if (TYPE == 'cunzhangbatv') {
         let url = `${BASEURL}/index.html`;
         superagent.get(url).end(function (err, sres) {
             // 常规的错误处理
@@ -33,46 +33,46 @@ router.get('/homeIndex',(req,res)=>{
             var str = sres.text;
             var $ = cheerio.load(str);
             //nav导航
-            let navs =[];
-            $('.type-slide').find('a').each((i,nav)=>{
+            let navs = [];
+            $('.type-slide').find('a').each((i, nav) => {
                 let $nav = $(nav);
                 let obj = {
-                    nav_name:$nav.text(),
-                    nav_h5_url:$nav.attr('href'),
-                    nav_modules:[]
+                    nav_name: $nav.text(),
+                    nav_h5_url: $nav.attr('href'),
+                    nav_modules: []
                 };
-                if(obj.nav_name !="APP下载" && obj.nav_name !="求片留言" ){
+                if (obj.nav_name != "APP下载" && obj.nav_name != "求片留言") {
                     navs.push(obj)
                 }
             });
             //首页数据
             let modules = [];
-            let pannels = [].slice.call($('.stui-pannel'),0).filter((item,i)=>{
+            let pannels = [].slice.call($('.stui-pannel'), 0).filter((item, i) => {
                 return $(item).find('.stui-vodlist').length > 0;
             });
-            pannels.forEach((pannel,i)=>{
+            pannels.forEach((pannel, i) => {
                 let obj = {};
                 let $pannel = $(pannel);
                 let $head = $pannel.find('.stui-pannel__head');
                 obj.module_name = $head.children('.title').text();
                 obj.module_icon = $head.children('.title').children('img').attr('src');
                 let $navTabs = $head.find('.nav-tabs');
-                if($navTabs && $navTabs.children('li').length > 0){
+                if ($navTabs && $navTabs.children('li').length > 0) {
                     obj.module_tabs = [];
-                    $navTabs.children('li').each((i,li)=>{
+                    $navTabs.children('li').each((i, li) => {
                         let tab_name = $(li).text();
                         let tab_id = $(li).children('a').attr('href');
                         let tab_movies = [];
-                        $pannel.find('.tab-content').children(tab_id).find('li').each((i,li)=>{
+                        $pannel.find('.tab-content').children(tab_id).find('li').each((i, li) => {
                             let $li = $(li);
                             let h5Detail = $li.find('.stui-vodlist__thumb').attr('href');
                             let reg = /(\d+)\.html$/;
                             let movie = {
-                                movie_h5_detail_url:h5Detail,
+                                movie_h5_detail_url: h5Detail,
                                 movie_id: reg.exec(h5Detail)[1],
-                                movie_img:$li.find('.stui-vodlist__thumb').attr('data-original'),
-                                movie_name:$li.find('.stui-vodlist__detail').find('.title').text(),
-                                movie_actors:$li.find('.stui-vodlist__detail').find('.text').text(),
+                                movie_img: $li.find('.stui-vodlist__thumb').attr('data-original'),
+                                movie_name: $li.find('.stui-vodlist__detail').find('.title').text(),
+                                movie_actors: $li.find('.stui-vodlist__detail').find('.text').text(),
                                 movie_pic_text: $li.find('.stui-vodlist__thumb').find('.pic-text').text(),
                             };
                             tab_movies.push(movie)
@@ -84,19 +84,19 @@ router.get('/homeIndex',(req,res)=>{
                         obj.module_tabs.push(tab)
                     });
                     obj.cur_tab_index = 0;
-                }else{
-                    obj.module_more_url=  $head.children('.more').attr('href');
+                } else {
+                    obj.module_more_url = $head.children('.more').attr('href');
                     obj.module_movies = [];
-                    $pannel.find('.stui-vodlist').find('li').each((i,li)=>{
+                    $pannel.find('.stui-vodlist').find('li').each((i, li) => {
                         let $li = $(li);
                         let h5Detail = $li.find('.stui-vodlist__thumb').attr('href');
                         let reg = /(\d+)\.html$/;
                         let movie = {
-                            movie_h5_detail_url:h5Detail,
+                            movie_h5_detail_url: h5Detail,
                             movie_id: reg.exec(h5Detail)[1],
-                            movie_img:$li.find('.stui-vodlist__thumb').attr('data-original'),
-                            movie_name:$li.find('.stui-vodlist__detail').find('.title').text(),
-                            movie_actors:$li.find('.stui-vodlist__detail').find('.text').text(),
+                            movie_img: $li.find('.stui-vodlist__thumb').attr('data-original'),
+                            movie_name: $li.find('.stui-vodlist__detail').find('.title').text(),
+                            movie_actors: $li.find('.stui-vodlist__detail').find('.text').text(),
                             movie_pic_text: $li.find('.stui-vodlist__thumb').find('.pic-text').text(),
                         };
                         obj.module_movies.push(movie)
@@ -105,12 +105,57 @@ router.get('/homeIndex',(req,res)=>{
                 modules.push(obj)
             });
             navs[0].nav_modules = modules;
-            res.send({code:1,data:{navs,cur_nav_index:0},msg:'success'})
+            res.send({code: 1, data: {navs, cur_nav_index: 0}, msg: 'success'})
         })
     }
-
-
 });
+
+/**
+ * 首页 其他nav的list
+ */
+router.get('/homeOtherNavIndex', (req, res) => {
+    if (TYPE == 'cunzhangbatv') {
+        let url = req.query.nav_h5_url;
+        superagent.get(url).end(function (err, sres) {
+            // 常规的错误处理
+            if (err) {
+                return next(err);
+            }
+            var str = sres.text;
+            var $ = cheerio.load(str);
+            //首页数据
+            let modules = [];
+            let pannels = $('.container').eq(1).children('.row').eq(1).children('.stui-pannel');
+            pannels = [].slice.call(pannels, 0);
+            pannels.forEach((pannel, i) => {
+                let obj = {};
+                let $pannel = $(pannel);
+                let $head = $pannel.find('.stui-pannel__head');
+                obj.module_name = $head.children('.title').text();
+                obj.module_icon = $head.children('.title').children('img').attr('src');
+                obj.module_more_url = $head.children('.more').attr('href');
+                obj.module_movies = [];
+                $pannel.find('.stui-vodlist').find('li').each((i, li) => {
+                    let $li = $(li);
+                    let h5Detail = $li.find('.stui-vodlist__thumb').attr('href');
+                    let reg = /(\d+)\.html$/;
+                    let movie = {
+                        movie_h5_detail_url: h5Detail,
+                        movie_id: reg.exec(h5Detail)[1],
+                        movie_img: $li.find('.stui-vodlist__thumb').attr('data-original'),
+                        movie_name: $li.find('.stui-vodlist__detail').find('.title').text(),
+                        movie_actors: $li.find('.stui-vodlist__detail').find('.text').text(),
+                        movie_pic_text: $li.find('.stui-vodlist__thumb').find('.pic-text').text(),
+                    };
+                    obj.module_movies.push(movie)
+                });
+                modules.push(obj)
+            });
+            res.send({code: 1, data: {nav_modules: modules}, msg: 'success'})
+        })
+    }
+});
+
 
 /**
  * 搜索
@@ -155,14 +200,14 @@ router.get('/search', function (req, res, next) {
                 };
                 list.push(obj);
             });
-            let result = {code: 1, data: {text,list}, msg: ''};
+            let result = {code: 1, data: {text, list}, msg: ''};
             if (list.length == 0) {
                 result.msg = '未搜索到相应视频资源'
             }
             res.send(result)
         })
     }
-    if(TYPE == 'cunzhangbatv'){
+    if (TYPE == 'cunzhangbatv') {
         let url = `${BASEURL}/vodsearch.html?wd=${encodeURIComponent(wd)}`;
         superagent.get(url).end(function (err, sres) {
 
@@ -177,13 +222,13 @@ router.get('/search', function (req, res, next) {
             let $list = $('.container').eq(1).find('.stui-pannel-box').eq(0).find('.stui-pannel_bd').find('.stui-vodlist__media');
             let text = $hd.find('.title').text();
             let list = [];
-            $list.find('li').each((i,v)=>{
+            $list.find('li').each((i, v) => {
                 let $v = $(v);
                 let $thumb = $v.find('.thumb').find('.stui-vodlist__thumb');
                 let $detail = $v.find('.detail');
                 let h5Detail = $thumb.attr('href');
                 let reg = /(\d+)\.html$/;
-                let reg2 =  /&#x7C7B;&#x578B;&#xFF1A;\<\/span\>([^<]*).*&#x5730;&#x533A;&#xFF1A;\<\/span\>([^<]*).*&#x5E74;&#x4EFD;&#xFF1A;\<\/span\>([^<]*)/;
+                let reg2 = /&#x7C7B;&#x578B;&#xFF1A;\<\/span\>([^<]*).*&#x5730;&#x533A;&#xFF1A;\<\/span\>([^<]*).*&#x5E74;&#x4EFD;&#xFF1A;\<\/span\>([^<]*)/;
                 let ary = reg2.exec($detail.find('.hidden-mi').html());
                 let obj = {
                     movie_h5_detail_url: h5Detail,//h5页面地址
@@ -197,7 +242,7 @@ router.get('/search', function (req, res, next) {
                 };
                 list.push(obj)
             });
-            res.send({code:1,data:{text,list},msg:'success'});
+            res.send({code: 1, data: {text, list}, msg: 'success'});
         });
     }
 });
@@ -265,7 +310,7 @@ router.get('/detail', function (req, res, next) {
             })
         })
     }
-    if(TYPE == 'cunzhangbatv'){
+    if (TYPE == 'cunzhangbatv') {
         let url = `${BASEURL}/v/${id}.html`;
         superagent.get(url).end(function (err, sres) {
 
@@ -282,7 +327,7 @@ router.get('/detail', function (req, res, next) {
             let $pannel0Box = $pannel0.find('.stui-pannel-box').children('div').eq(1);
             let $thumb = $pannel0Box.find('.stui-content__thumb');
             let movie_img = $thumb.find('img').attr('src');
-            let movie_pic_text =$thumb.find('.pic-text').text();
+            let movie_pic_text = $thumb.find('.pic-text').text();
             let movie_title = $thumb.find('a').attr('title');
             let $detail = $pannel0Box.find('.stui-content__detail');
             let movie_score = $detail.find('.score').text();
@@ -306,25 +351,25 @@ router.get('/detail', function (req, res, next) {
                 movie_desc: movie_desc,
                 movie_players: [],
                 guess_you_like: [],
-                movie_score:movie_score,
-                movie_update_time:movie_update_time,
+                movie_score: movie_score,
+                movie_update_time: movie_update_time,
             };
 
             //播放信息
             let $playList = $div.find('.playlist');
-            $playList.each((i,v)=>{
+            $playList.each((i, v) => {
                 let $v = $(v);
                 let obj = {
-                    player_platform:$v.find('.stui-pannel_hd').find('.title').text().replace('村长爸','电影街'),
-                    player_list:[]
+                    player_platform: $v.find('.stui-pannel_hd').find('.title').text().replace('村长爸', '电影街'),
+                    player_list: []
                 };
-                $v.find('.stui-pannel_bd').find('li').each((n,li)=>{
+                $v.find('.stui-pannel_bd').find('li').each((n, li) => {
                     let $li = $(li);
                     let movie_player_url = $li.find('a').attr('href');
                     let idStr = movie_player_url.split('/')[movie_player_url.split('/').length - 1];
                     let reg = /([^\s]*)\.html/;
                     let item = {
-                        title:$li.text(),
+                        title: $li.text(),
                         movie_player_url,
                         movie_player_id: reg.test(idStr) ? reg.exec(idStr)[1] : ''
                     };
@@ -333,12 +378,12 @@ router.get('/detail', function (req, res, next) {
                 detail.movie_players.push(obj);
             });
             //猜你喜欢
-            if($("#desc").next().length >0){
+            if ($("#desc").next().length > 0) {
                 let $youlike = $("#desc").next().children('.stui-pannel-box');
-                if($youlike.length>0){
+                if ($youlike.length > 0) {
                     let $hd = $youlike.children('.stui-pannel_hd');
-                    let $list =  $youlike.children('.stui-pannel_bd').children('.stui-vodlist__bd').children('li');
-                    $list.each((v,li)=>{
+                    let $list = $youlike.children('.stui-pannel_bd').children('.stui-vodlist__bd').children('li');
+                    $list.each((v, li) => {
                         let $li = $(li);
                         let movie_h5_url = $li.find('.stui-vodlist__thumb').attr('href');
                         let movie_img = $li.find('.stui-vodlist__thumb').attr('data-original');
@@ -356,7 +401,7 @@ router.get('/detail', function (req, res, next) {
                     })
                 }
             }
-            res.send({code:1,data:{detail},msg:'success'});
+            res.send({code: 1, data: {detail}, msg: 'success'});
         });
     }
 });
@@ -372,18 +417,18 @@ router.get('/getPlayerSource', function (req, res, next) {
     if (TYPE == '1026tv') {
         url = `${BASEURL}/kan/${movie_player_id}.html`;
     }
-    if(TYPE == 'cunzhangbatv'){
-        url =`${BASEURL}/play/${movie_player_id}.html`
+    if (TYPE == 'cunzhangbatv') {
+        url = `${BASEURL}/play/${movie_player_id}.html`
     }
     let config = require(`../common/${TYPE}.json`);
-    if(config[movie_player_id]){
+    if (config[movie_player_id]) {
         console.log(1)
         let player_info = config[movie_player_id];
         console.log(player_info)
         res.send({
             code: 1, data: {player_info}, msg: ''
         });
-    }else{
+    } else {
         console.log(2)
         puppeteer.launch({
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
@@ -410,7 +455,7 @@ router.get('/getPlayerSource', function (req, res, next) {
             browser.close();
             //做缓存
             config[movie_player_id] = player_info;
-            fs.writeFile(path.join(__dirname,`../common/${TYPE}.json`), JSON.stringify(config),'utf8',function(error){
+            fs.writeFile(path.join(__dirname, `../common/${TYPE}.json`), JSON.stringify(config), 'utf8', function (error) {
                 res.send({
                     code: 1, data: {player_info}, msg: ''
                 });
@@ -433,14 +478,14 @@ function resChinese(text) {
  * 补协议和domain
  * @type {Router}
  */
-function hostCheck(str){
-   let host = `${BASEURL}`;
-   let reg = /^(?=^.{3,255}$)(http(s)?:\/\/)?(www\.)?[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+(:\d+)*(\/\w+\.\w+)*/
-   if(!reg.test(str)){
+function hostCheck(str) {
+    let host = `${BASEURL}`;
+    let reg = /^(?=^.{3,255}$)(http(s)?:\/\/)?(www\.)?[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+(:\d+)*(\/\w+\.\w+)*/
+    if (!reg.test(str)) {
         return `${host}${str}`
-   }else{
-       return  str;
-   }
+    } else {
+        return str;
+    }
 }
 
 module.exports = router;
